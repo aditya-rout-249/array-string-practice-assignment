9d5041501b3c7b68f891008daa289586620de144
aryan_249 <asoni@qodeleaf.com>
2022-11-02 11:28:22 +0530

    Use Cases of Array.from methods with callbacks .one methods creates simple array .second methods taske iterable array and runs callback and forms an array

--- arrayMethods.js
+++ arrayMethods.js
@@ -5,3 +5,27 @@ const beers = ["budweiser magmun", "Kf", "calsberg", "corona", "bira"];
 
 // using push method to add beer brand
 beers.push("lemount");
+
+// use cases of array.from method
+// an object to be passed in array.from
+const employes = {
+  id1: {
+    name: "Vinod",
+  },
+  id2: {
+    name: "Abbas",
+  },
+};
+
+
+// simple Array to have employe details as empDetails
+const empDetails = Array.from(employes)
+console.log(empDetails)   
+
+// using from with callback function
+const quantity = Array.from(beers, (beer, index) => {
+  return beer + ":" + index;
+});
+console.log(quantity);
+
+// use
