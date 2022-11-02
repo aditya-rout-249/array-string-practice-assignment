231363fb2fb3bbb9c0d79ee88b75eef0c04edef6
Aryan_249 <asoni@qodeleaf.com>
2022-11-02 10:34:05 +0530

    Revert "Implemented an array named beers with use of push method"
    This reverts commit a31c45379a1ad79ae2bc7a7eae10741ed6a03f8d.

--- arrayMethods.js
+++
@@ -1,7 +0,0 @@
-// includes  problems solved on array using different array methods
-
-// Using random array of name beers
-const beers = ["budweiser magmun", "Kf", "calsberg", "corona", "bira"];
-
-// using push method to add beer brand
-beers.push("lemount");
