a31c45379a1ad79ae2bc7a7eae10741ed6a03f8d
Aryan_249 <asoni@qodeleaf.com>
2022-11-02 10:33:31 +0530

    Implemented an array named beers with use of push method

---
+++ arrayMethods.js
@@ -0,0 +1,7 @@
+// includes  problems solved on array using different array methods
+
+// Using random array of name beers
+const beers = ["budweiser magmun", "Kf", "calsberg", "corona", "bira"];
+
+// using push method to add beer brand
+beers.push("lemount");
