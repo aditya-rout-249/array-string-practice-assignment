0ad4e56b38689bb5ec844f15af724a98a0c9b66b
aryan_249 <asoni@qodeleaf.com>
2022-11-02 16:59:37 +0530

    initialized a stringMethods.js file that consist of an initial string name das namedString and a use case namedString.length

---
+++ stringMethods.js
@@ -0,0 +1,7 @@
+// this programme consist of all the javaScript use cases of all the string methods
+
+//Varibale string named as namedString
+const namedString = "aryan249 is github user name"
+
+//Length of namedString variable
+console.log(namedString.length)
